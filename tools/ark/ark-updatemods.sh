39654a71cde98d2072fb85e407d7291fec408a24
ericmathison007 <ericmathison20@gmail.com>
2017-12-06 12:45:24 -0500

    Update ark-updatemods.sh

--- tools/ark/ark-updatemods.sh
+++ tools/ark/ark-updatemods.sh
@@ -10,6 +10,9 @@ mods=( 630601751 693416678 708807240 731604991 793605978 895711211 924933745 )
 # ARK installation dir
 arkdir="/home/ark/server"
 
+# Location of z_unpack.py
+zdir="/home/ark"
+
 rm -rf $arkdir/steamapps/workshop/appworkshop_346110.acf
 
 for i in "${mods[@]}"
@@ -23,13 +26,15 @@ do
         /usr/games/steamcmd +login anonymous +force_install_dir $arkdir +workshop_download_item 346110 $i +quit
 done
 
+# Move latest mod files to ARK mods directory
 mv $arkdir/steamapps/workshop/content/346110/* $arkdir/ShooterGame/Content/Mods/
-
-rm -rf $arkdir/steamapps/workshop/content/346110
 
 # Decompress ARK zlib files
 for file in $(find "$arkdir/ShooterGame/Content/Mods" -type f -name '*.z')
 do
-        python /home/ark/z_unpack.py $file "${file%.*}"
+        # Run decompress script and remove the .z file extension on matching files
+        python $zdir/z_unpack.py $file "${file%.*}"
+        
+        # Remove compressed and related files
         rm -rf $file $file.uncompressed_size
 done
